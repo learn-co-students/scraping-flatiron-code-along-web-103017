ad364c0e77a9bd06be1d339eb8b65812fc9dcd43
psarma89 <priyam.sarma@gmail.com>
2017-11-05 22:18:03 -0500

    Adds solution for Flatiron scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +16,24 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+    html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses')
+    doc = Nokogiri::HTML(html)
+  end
+
+  def get_courses
+    self.get_page.css(".post")
+    # binding.pry
+  end
+
+  def make_courses
+    self.get_courses.each do |course|
+      new_instance = Course.new
+      new_instance.title = course.css("h2").text
+      new_instance.schedule = course.css("em").text
+      new_instance.description = course.css("p").text
+    end
+  end
+
 end
-
-
-
